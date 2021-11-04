e855462ab0618515cf72732a2779dc8b650f9abd
kacperxawer <kacperxawer.123@gmail.com>
2021-11-04 15:37:27 +0100

    Fragment3 z obsluga dekrementacji zmiennej

--- app/src/main/java/com/example/myfrags/Fragment3.java
+++ app/src/main/java/com/example/myfrags/Fragment3.java
@@ -4,10 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 public class Fragment3 extends Fragment {
+
+    private TextView text;
+    private Button button;
+    private FragsData fragsData;
+    private Observer<Integer> numberObserver;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +27,26 @@ public class Fragment3 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_3, container, false);
+
+        text = (TextView) view.findViewById(R.id.wartosc);
+        button = (Button) view.findViewById(R.id.button_decrease);
+        fragsData = new ViewModelProvider(requireActivity()).get(FragsData.class);
+        numberObserver = new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer newInteger) {
+                text.setText(newInteger.toString());
+            }
+        };
+        fragsData.counter.observe(getViewLifecycleOwner(), numberObserver);
+        button.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View view) {
+                                          Integer i = fragsData.counter.getValue();
+                                          fragsData.counter.setValue(--i);
+                                      }
+                                  }
+        );
+
 
         return view;
     }
